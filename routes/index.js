2d8a42efed65673017bf2033b3a623e88c0bdab2
vansh deep Arya <92106532+Vansh432@users.noreply.github.com>
2023-05-31 17:08:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require('express');//express module-->
 
-const router = express.Router();
-const home_controller = require('../controller/home_controller')
+const router = express.Router();//create new Router Object-->
+const home_controller = require('../controller/home_controller')//HomeController modules-->
 
-router.get('/', home_controller.home);
-router.get('/create_project', home_controller.createProject);
-router.post('/projectSet', home_controller.project);
-router.get('/projectview', home_controller.projectview);
-router.get('/projectpage', home_controller.projectpage);
-router.get('/create_issue', home_controller.createissue);
-router.post('/new_issue_created', home_controller.issuecreated);
-router.get('/delete_issue', home_controller.delete_issue);
-router.use('/filter', require('./filter'));
+router.get('/', home_controller.home);//home page controller-->
+router.get('/create_project', home_controller.createProject);//create project page controller-->
+router.post('/projectSet', home_controller.project);//project set controller-->
+router.get('/projectview', home_controller.projectview);//project view controller-->
+router.get('/projectpage', home_controller.projectpage);//project page controller-->
+router.get('/create_issue', home_controller.createissue);// create issue controller-->
+router.post('/new_issue_created', home_controller.issuecreated);//new issue created controller-->
+router.get('/delete_issue', home_controller.delete_issue);//delete issue controller-->
 
-
-module.exports = router;
+module.exports = router;//module.exports = router;
