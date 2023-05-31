4ce526974a3db1529bdfaec4b15863646cac76a6
vansh deep Arya <92106532+Vansh432@users.noreply.github.com>
2023-05-31 17:10:57 +0530

    Update creatIssue.js

--- assets/script/creatIssue.js
+++ assets/script/creatIssue.js
@@ -1,10 +1,16 @@
 //show the dropdown list of issues-->
 let heading = document.getElementById('labelHeading');
 let labelList = document.getElementById('labelList');
+
+
+//when user click on label heading line no 37 in project_view.ejs files toggle class -->
 heading.addEventListener('click', () => {
   labelList.classList.toggle('active');
 })
 
+
+
+//we add checked in the checkboxe when click on labels
 let Li = document.querySelectorAll('li');
 Li.forEach(Element => {
   Element.addEventListener('click', () => {
