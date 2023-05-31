6da2818f673f98ab1b2c1c88b917f4dba65d7db8
vansh deep Arya <92106532+Vansh432@users.noreply.github.com>
2023-05-31 17:07:53 +0530

    Delete filter.js

--- routes/filter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const filterController = require('../controller/filter_controller');
-
-router.get('/', filterController.filter);
-
-module.exports = router;
